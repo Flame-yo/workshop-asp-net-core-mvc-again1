6fcd7b6a567656dafc8306b698bf4c9d4df34885
Gustavo Henrique <yoflame99@gmail.com>
2024-12-10 23:36:44 -0300

    Other entities and second migration

---
+++ SalesWebMvc1/Models/Enums/SaleStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc1.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Pending = 0,
+        Billed = 0,
+        Canceled = 0
+    }
+}
