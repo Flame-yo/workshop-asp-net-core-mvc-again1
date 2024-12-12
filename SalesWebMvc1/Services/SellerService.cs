d280a6d037eb0e222bd849814ba81e99f9e9c9a2
Gustavo Henrique <yoflame99@gmail.com>
2024-12-11 22:11:20 -0300

    SellersController

---
+++ SalesWebMvc1/Services/SellerService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc1.Services
+{
+    public class SellerService
+    {
+    }
+}
